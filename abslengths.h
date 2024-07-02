b790a5a3d9796be6bee0f2cc2c69f927fa0dbbf9
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-02 16:13:47 -0400

    Changed functionality

--- abslengths.h
+++ abslengths.h
@@ -3,16 +3,16 @@
 namespace Unknotter {
     //A length of consecutive crossovers.
     struct AbsLength final {
-        //The inclusive lower bound of the range. If index1 > index2, the range will wrap around.
+        //The inclusive lower bound of the range. If index1 > index2, the range will wrap around. If index1 == index2, the range will encompass all points.
         size_t index1;
-        //The exclusive upper bound of the range. If index1 > index2, the range will wrap around.
+        //The exclusive upper bound of the range. If index1 > index2, the range will wrap around. If index1 == index2, the range will encompass all points.
         size_t index2;
         //Initializes an AbsLength instance.
         constexpr __forceinline AbsLength(size_t Index1, size_t Index2)
             : index1(Index1), index2(Index2) { }
         //Returns true if a given index is within the range, false otherwise.
         constexpr __forceinline bool InRange(size_t Index) const {
-            if (index1 <= index2) {
+            if (index1 < index2) {
                 return (Index >= index1 && Index < index2);
             }
             else {
@@ -25,7 +25,7 @@ namespace Unknotter {
         }
         //Computes the amount of indicies in the range.
         constexpr __forceinline size_t RangeSize(size_t TotalSize) const {
-            if (index1 <= index2) {
+            if (index1 < index2) {
                 return index2 - index1;
             }
             else {
