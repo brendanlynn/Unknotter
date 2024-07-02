10ca2bfb94645ab38c057711769a4a801ce0e80e
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-02 12:36:57 -0400

    Added absknot.*

---
+++ absknot.cpp
@@ -0,0 +1,57 @@
+#include "absknot.h"
+
+#include <algorithm>
+
+void subtractByList(size_t& Index, size_t* Array, size_t Length) {
+    size_t lwr = 0;
+    size_t upr = Length;
+    while (upr - lwr > 1) {
+        size_t avg = (upr + lwr) >> 1;
+        if (Array[avg] >= Index) {
+            lwr = avg;
+        }
+        else {
+            upr = avg;
+        }
+    }
+    Index -= lwr;
+}
+
+void Unknotter::AbsKnot::RemoveAt(size_t Index) {
+    size_t indicies[2];
+    indicies[0] = Index;
+    indicies[1] = crosses[Index].crossingIndex;
+    RemoveAtFull(indicies, 2);
+}
+void Unknotter::AbsKnot::RemoveAt(size_t* Indicies, size_t IndexCount) {
+    std::vector<size_t> arr(IndexCount);
+    memcpy(arr.data(), Indicies, IndexCount * sizeof(size_t));
+    std::sort(arr.data(), arr.data() + IndexCount); //ascending
+    size_t* ep = arr.data() + IndexCount;
+    for (size_t i = 0; i < IndexCount; ++i) {
+        if (!std::binary_search(arr.data(), ep, i)) {
+            arr.push_back(i);
+        }
+    }
+    RemoveAtFull(arr.data(), arr.size());
+}
+void Unknotter::AbsKnot::RemoveAtFull(size_t* Indicies, size_t IndexCount) {
+    if (!IndexCount) {
+        return;
+    }
+    if (IndexCount & 1) {
+        throw std::exception("It is impossible for all nodes present to have their corresponding node also present when IndexCount is odd.");
+    }
+    std::vector<size_t> arr(IndexCount);
+    memcpy(arr.data(), Indicies, IndexCount * sizeof(size_t));
+    std::sort(arr.data(), arr.data() + IndexCount); //ascending
+    size_t c = arr[0];
+    for (size_t i = 1; i < IndexCount; ++i) {
+        size_t s = arr[i - 1] + 1;
+        size_t l = arr[i] - s;
+        memcpy(crosses.data() + c, crosses.data() + s, sizeof(AbsCross) * l);
+    }
+    for (AbsCross& cross : crosses) {
+        subtractByList(cross.crossingIndex, arr.data(), IndexCount);
+    }
+}
---
+++ absknot.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+namespace Unknotter {
+    //A node of AbsCross.
+    struct AbsCross final {
+        //The index from the other segment's perspective.
+        size_t crossingIndex;
+        //Whether this segment is going over or under the other segment whose index is crossingIndex.
+        bool over;
+    };
+    //A representation of a mathematical knot, with all crossovers in an array with each node storing the index of the corresponding node.
+    struct AbsKnot final {
+        std::vector<AbsCross> crosses;
+        //Removes the crossover indicated by the index. Two nodes will be removed in total, the one referenced, and the one corresponding.
+        void RemoveAt(size_t Index);
+        //Removes the crossovers indicated by the indicies. If a node is present without it's corresponding node, the corresponding node will also be removed.
+        void RemoveAt(size_t* Indicies, size_t IndexCount);
+        //Removes the crossovers indicated by the indicies. The function assumes that all nodes in the list also have their corresponding node in the list. If this assumption is false, the behavior of the function is undefined, and may corrupt the class.
+        void RemoveAtFull(size_t* Indicies, size_t IndexCount);
+        //The number of crossovers. The number of nodes is always twice the number of crossovers.
+        inline size_t CrossoverCount() const {
+            return crosses.size() >> 1;
+        }
+    };
+}
