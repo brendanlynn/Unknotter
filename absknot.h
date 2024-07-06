a603f6d2c804cedcbd68ecbc40b1fdb57bf06ec6
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-05 20:51:16 -0400

    Fixed minor documentation terminology mismatch

--- absknot.h
+++ absknot.h
@@ -18,7 +18,7 @@ namespace Unknotter {
         void RemoveAt(size_t Index);
         //Removes the crossovers indicated by the indicies. If a node is present without it's corresponding node, the corresponding node will also be removed.
         void RemoveAt(const size_t* Indicies, size_t IndexCount);
-        //Removes the crossovers indicated by the indicies. The function assumes that all nodes in the list also have their corresponding node in the list. If this assumption is false, the behavior of the function is undefined, and may corrupt the class.
+        //Removes the crossovers indicated by the indicies. The function assumes that all nodes in the list also have their corresponding node in the list. If this assumption is false, the behavior of the function is undefined, and may corrupt the struct.
         void RemoveAtFull(const size_t* Indicies, size_t IndexCount);
         //Removes the crossovers in the given range. Both the nodes in the range and the nodes corresponding to nodes in the range will be removed. The lower bound of the range is inclusive; the upper bound of the range is not.
         void RemoveRange(AbsLength Range);
