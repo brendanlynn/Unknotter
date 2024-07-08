14a39d49c873ecf225a02524b4a7dec7891a0756
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-08 10:26:05 -0400

    Aligned inherited constructors

--- abscircle.h
+++ abscircle.h
@@ -16,10 +16,10 @@ namespace Unknotter {
 
 constexpr __forceinline Unknotter::AbsCircle::AbsCircle(size_t Index11, size_t Index12, size_t Index21, size_t Index22)
     : l1(Index11, Index12),
-    l2(Index21, Index22) { }
+      l2(Index21, Index22) { }
 constexpr __forceinline Unknotter::AbsCircle::AbsCircle(size_t Index1, size_t Index2, AbsKnot& Knot)
     : l1(Index1, Index2),
-    l2(Knot[Index1].crossingIndex, Knot[Index2].crossingIndex) { }
+      l2(Knot[Index1].crossingIndex, Knot[Index2].crossingIndex) { }
 constexpr __forceinline Unknotter::AbsCircle::AbsCircle(AbsLength Length, AbsKnot& Knot)
     : AbsCircle(Length.index1, Length.index2, Knot) { }
 constexpr __forceinline bool Unknotter::AbsCircle::OnCircle(size_t Index) const {
